9007a1e44b08d0e003095da3a258fd190e4de0a1
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-02-20 11:16:43 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 function welcomeMessage()
 {
-alert("Welcome to the new Fots website. It is still being updated so check back later.");
+alert("Welcome to the new Fots website. It is still being updated so check back later. By clicking "OK" you accept that you may be exposed to Fots including the Rice Crispy Fot, the olive Fot, and the cereal Fot. Also you are accepting that all Fots are dry and that you drink them.");
 }
