b13a0eabab794271bf552c0bf7dd793ac9be6436
LB123658 <79328251+LB123658@users.noreply.github.com>
2021-06-25 08:49:17 -0500

    Create fotsapp.cpp

---
+++ fotsapp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "THE STORY OF FOTS\n\nFood + shots = FOTS\nFots are dry crushed food in shot glasses that you drink.\nIt is drinking and eating cimbined into one - Fotting.\nThe glasses that fots are in are called fot glasses\nTypes of common fots include:\nThe Rispe™ Fot, which is rice crisipies,\nThe Olive Fot, which is crushed olives (one of the only wet fots),\nThe Meat Fot of pulped meat,\nand The Orea Fot made of crushed Oreos.\n\nLearn more about Fots at lb123658.github.io/Fots\n\n\nMore Fotting\nWhat a fot glass looks like\n......\n.    .\n.    .\n.    .\n.    .\n.    .\n.    .\n......";
+  return 0;
+}
